e6097b6848d46e1a36fa401afe23457843a3e6b5
Iryna Feuerstein <iryna.feuerstein@prodyna.com>
2016-10-02 16:22:59 +0200

    Second plot

--- plot2.R
+++ plot2.R
@@ -1,7 +1,7 @@
 png(filename="plot2.png")
 all_data<-read.csv(file = "household_power_consumption.txt", header = TRUE, sep = ";", na.strings = c("?"))
 data <- subset(all_data, Date == '1/2/2007' | Date == '2/2/2007')
-
-
-
+date_time <- paste(data$Date, data$Time)
+date_time <- strptime(date_time, format = "%d/%m/%Y %H:%M:%S")
+plot(date_time, data$Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
 dev.off()
