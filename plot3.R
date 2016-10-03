ad72079ad3311f07b94b799c643fde709ab92678
Iryna Feuerstein <iryna.feuerstein@prodyna.com>
2016-10-03 06:53:48 +0200

    Third plot

--- plot3.R
+++ plot3.R
@@ -8,6 +8,6 @@ date_time <- strptime(date_time, format = "%d/%m/%Y %H:%M:%S")
 plot(date_time, data$Sub_metering_1, type = "l", ylab = "Energy sub metering", xlab="")
 lines(date_time, data$Sub_metering_2, col="red")
 lines(date_time, data$Sub_metering_3, col="blue")
-legend("topright", pch=c("-","-","-"), col=c("black", "red", "blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_2"))
+legend("topright", lty=c(1,1,1), col=c("black", "red", "blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_2"))
 
 dev.off()
